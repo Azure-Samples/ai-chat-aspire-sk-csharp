41f73a9cbabd4303b9ebbd0109ef91b972da11d6
Jordan Matthiesen <1333029+jmatthiesen@users.noreply.github.com>
2024-10-17 23:12:24 -0700

    Addressing some more feedback, and fixing the app so that it can auto-provision the OpenAI dependency.

--- src/AIChatApp.AppHost/Program.cs
+++ src/AIChatApp.AppHost/Program.cs
@@ -4,15 +4,13 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-// Add support for a local configuration file, which doesn't get committed to source control
-builder.Configuration.Sources.Insert(0, new JsonConfigurationSource { Path = "appsettings.Local.json", Optional = true });
-
 var chatDeploymentName = "chat";
 var connectionString = builder.Configuration.GetConnectionString("openai");
+
 var openai = String.IsNullOrEmpty(connectionString)
     ? builder.AddAzureOpenAI("openai")
          .AddDeployment(new AzureOpenAIDeployment(chatDeploymentName, "gpt-4o", "2024-05-13", "GlobalStandard", 10))
-    : builder.AddConnectionString("openai", "OPENAI_CONNECTION_STRING");
+    : builder.AddConnectionString("openai");
 
 builder.AddProject<AIChatApp_Web>("aichatapp-web")
     .WithReference(openai)
